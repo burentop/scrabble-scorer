3e00ca2323179037c11f1181d26be3c0957057bd
Brent Perry <burentoperii@gmail.com>
2016-12-28 11:09:38 -0600

    complete user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,14 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    Console myConsole = System.console();
+    Scrabble myScrabble = new Scrabble();
+
+    System.out.println("Enter a word, and I will tell you the Scrabble points for it: ");
+    String inputWord = myConsole.readLine();
+
+    Integer wordScore = myScrabble.calculateScore(inputWord);
+    System.out.println(inputWord + " would score " + wordScore + " points in Scrabble.");
   }
 }
