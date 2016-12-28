20d6abf6b061ad40c98296b3fc55f70a2be7be0c
Brent Perry <burentoperii@gmail.com>
2016-12-28 10:23:22 -0600

    pass 3rd test, assign points to 3-point letters.

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -3,6 +3,8 @@ public class Scrabble {
   public Integer getLetterScore(String letter) {
     if (letter == "d" || letter == "g") {
       return 2;
+    } else if (letter == "b" || letter == "c" || letter == "m" || letter == "p") {
+      return 3;
     } else {
       return 1;
     }
